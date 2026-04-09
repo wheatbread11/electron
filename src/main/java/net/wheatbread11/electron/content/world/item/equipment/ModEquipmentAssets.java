ca1bd08730c22adf68599265a9a662b3ebbee4e7
komugi_bread <kazuya1101.personal@gmail.com>
2026-04-09 17:25:08 +0900

    move namespace of equipment assets

--- src/main/java/net/wheatbread11/electron/content/world/item/equipment/ModEquipmentAssets.java
+++ src/main/java/net/wheatbread11/electron/content/world/item/equipment/ModEquipmentAssets.java
@@ -4,12 +4,13 @@ import net.minecraft.core.Registry;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.equipment.EquipmentAsset;
+import net.wheatbread11.electron.Electron;
 
 public interface ModEquipmentAssets {
     ResourceKey<? extends Registry<EquipmentAsset>> ROOT_ID = ResourceKey.createRegistryKey(Identifier.withDefaultNamespace("equipment_asset"));
     ResourceKey<EquipmentAsset> SILVER = createId("silver");
 
     static ResourceKey<EquipmentAsset> createId(String name) {
-        return ResourceKey.create(ROOT_ID, Identifier.withDefaultNamespace(name));
+        return ResourceKey.create(ROOT_ID, Identifier.fromNamespaceAndPath(Electron.MOD_ID, name));
     }
 }
